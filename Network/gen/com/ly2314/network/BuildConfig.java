b5c8f17788ad72a1b399671e2a767c5604ecd2ba
Ting-Yuan Hsia <ly2314@gmail.com>
2014-05-12 19:52:23 +0800

    New project
    1. StrictMode
    2. URLConnection

---
+++ Network/gen/com/ly2314/network/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ly2314.network;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Network/src/com/ly2314/network/MainActivity.java
@@ -0,0 +1,111 @@
+package com.ly2314.network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.os.Build;
+
+@SuppressLint("NewApi")
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+        
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+    	private TextView _textView;
+    	
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            _textView = (TextView) rootView.findViewById(R.id.textView1);
+            String content = fetch();
+            _textView.setText(content);
+            return rootView;
+        }
+        
+        private String fetch()
+        {
+        	String urlString = "https://www.ly2314.cc";
+        	URL url;
+			try {
+				url = new URL(urlString);
+	        	URLConnection urlConnection = url.openConnection();
+	        	InputStream is = urlConnection.getInputStream();
+	        	BufferedReader buffer = new BufferedReader(new InputStreamReader(is));
+	        	String result = "", line;
+	        	while ((line = buffer.readLine()) != null)
+	        	{
+	        		result += line;
+	        	}
+	        	Log.d("debug", result);
+	        	return result;
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+        }
+    }
+
+}
